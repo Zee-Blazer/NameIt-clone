7ef0b893a36966d664b6c5d1cb9c55744c37a5a5
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2026-06-16 13:42:21 +0100

    Stopped at some point

--- src/component/homeScreenComp/playwithfriend.comp.js
+++ src/component/homeScreenComp/playwithfriend.comp.js
@@ -1,4 +1,10 @@
 
+// MUI component
+import Switch from '@mui/material/Switch';
+import Button from '@mui/material/Button';
+
+const label = { slotProps: { input: { 'aria-label': 'Switch demo' } } };
+
 const PlayWithFriendComp = () => {
 
     return (
@@ -8,7 +14,28 @@ const PlayWithFriendComp = () => {
                 type="name" 
                 placeholder="What's your name?" 
             />
-            <div>Play with friends</div>
+
+            <div className='flex justify-between items-center'>
+                <p className='font-bold'>Make room public</p>
+                <Switch {...label} />
+            </div>
+
+            <Button 
+                variant="contained"
+                sx={{ width: "100%", paddingY: 2, background: "red", marginY: 2.5 }}
+            >Create Room</Button>
+
+            <p 
+                className='text-sm text-center text-gray-500'
+            >Private room — share the link with friends</p>
+            <p
+                className='text-sm text-center text-gray-500'
+            >or join with a code</p>
+
+            <Button 
+                variant="outlined"
+                sx={{ width: "100%", color: "red", paddingY: 1, marginY: 2.5, borderColor: "red" }}
+            >Join a Room</Button>
         </div>
     )
 }
--- src/screens/home.screen.js
+++ src/screens/home.screen.js
@@ -11,7 +11,6 @@ const HomeScreen = () => {
             <NavBarComp />
             <DescripComp />
             <PlayConsoleComp />
-            Home Screen
         </>
     )
 }
